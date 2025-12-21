3ab86ceb5240f956a51ecf9cd66568b8289383b1
Ilia Zharikov <iljarikov@gmail.com>
2025-12-21 15:36:33 +0100

    fix: improve Lemon Squeezy script initialization for overlay

--- frontend/src/components/layout/lemon-squeezy-script.tsx
+++ frontend/src/components/layout/lemon-squeezy-script.tsx
@@ -1,8 +1,18 @@
 "use client";
 
 import Script from "next/script";
+import { useEffect } from "react";
 
 export function LemonSqueezyScript() {
+    useEffect(() => {
+        // If navigating back to the page and script is already there
+        // @ts-ignore
+        if (window.LemonSqueezy) {
+            // @ts-ignore
+            window.LemonSqueezy.Setup();
+        }
+    }, []);
+
     return (
         <Script
             src="https://assets.lemonsqueezy.com/lemon.js"
@@ -10,9 +20,13 @@ export function LemonSqueezyScript() {
             onLoad={() => {
                 console.log("Lemon Squeezy script loaded");
                 // @ts-ignore
-                if (window.createLemonSqueezy) {
+                if (window.LemonSqueezy) {
                     // @ts-ignore
-                    window.createLemonSqueezy();
+                    window.LemonSqueezy.Setup({
+                        eventHandler: (event: any) => {
+                            console.log('Lemon Squeezy Event:', event);
+                        }
+                    });
                 }
             }}
         />
